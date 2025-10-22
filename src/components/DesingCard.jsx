94dc374ba24d287db59913c7f67ae5ec4f7276d1
GiddonPk <jhg0015@alu.medac.es>
2025-10-22 19:34:01 +0200

    rounded

--- src/components/DesingCard.jsx
+++ src/components/DesingCard.jsx
@@ -6,7 +6,7 @@ export default function DesignCard({ src, alt }) {
 			<img
 				src={src}
 				alt={alt}
-				className='w-80 h-80 rounded-4xl bg-white shadow-lg hover:scale-105 transition-transform duration-300 object-contain border-3'
+				className='w-80 h-80 rounded-lg bg-white shadow-lg hover:scale-105 transition-transform duration-300 object-contain border-3'
 			/>
 		</div>
 	);
